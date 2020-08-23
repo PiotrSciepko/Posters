097c43d5f352776793765813893829aebfe9bc21
piotrs <piotrek.priv@gmail.com>
2020-08-23 23:18:16 +0200

    add User Input

--- src/com/PiotrS/Poster.java
+++ src/com/PiotrS/Poster.java
@@ -3,6 +3,7 @@ package com.PiotrS;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Poster {
 
@@ -31,7 +32,7 @@ public class Poster {
     }
 
     public void reduceZeros(List<Integer> list) {
-        stripZeros(list);
+        //stripZeros(list);
         List<Integer> indexOfZeros = findZeros(list);
         int rem = 0;
         if (indexOfZeros.size() > 1) {
@@ -65,22 +66,22 @@ public class Poster {
     }
 
     public static void main(String[] args) {
-        /*List<Integer> l = new ArrayList<>();
-        try (FileReader f = new FileReader("pla1a.in")) {
-            try (Scanner s = new Scanner(f)) {
-                while (s.hasNext()) {
-                    l.add(s.nextInt());
-                }
-            }
-        }*/
 
+        List<Integer> arr = new ArrayList<>();
 
-        List<Integer> arr = new ArrayList<>(List.of(10, 9, 10, 10, 23, 21, 20, 23, 124, 23, 20, 23, 23, 24, 23, 120, 23, 15, 12, 14, 15, 14, 13, 12, 14, 14, 13, 14, 23, 24, 28, 27, 28, 29, 30, 29, 311, 29, 23, 18, 17, 16, 15, 15, 15, 17, 10, 11, 10, 9, 8, 8, 10, 18, 19, 17, 14, 20, 14, 21));
-        //List<Integer> arr = new ArrayList<>(List.of(100, 3, 0, 0, 1, 0, 0, 0, 0, 0, 0, 9));
+        Scanner sc = new Scanner(System.in);
+        int limit = sc.nextInt();
+        for (int i = 0; i < limit; i++) {
+            //sc.nextInt();
+            arr.add(sc.nextInt());
+        }
+
+        //List<Integer> arr = new ArrayList<>(List.of(10, 9, 10, 10, 23, 21, 20, 23, 124, 23, 20, 23, 23, 24, 23, 120, 23, 15, 12, 14, 15, 14, 13, 12, 14, 14, 13, 14, 23, 24, 28, 27, 28, 29, 30, 29, 311, 29, 23, 18, 17, 16, 15, 15, 15, 17, 10, 11, 10, 9, 8, 8, 10, 18, 19, 17, 14, 20, 14, 21));
+        //List<Integer> arr = new ArrayList<>(List.of(0,0,100, 3, 0, 0, 1, 0, 0, 0, 0, 0, 0, 9,0,0,0));
         //List<Integer> arr = new ArrayList<>(List.of(5,2,4,3,2,3,8));
 
         Poster p = new Poster();
-        //p.stripZeros(arr);
+        p.stripZeros(arr);
         //p.arrangePosters(arr);
         ///*
         //while (Collections.frequency(arr, 0) != arr.size()) {
